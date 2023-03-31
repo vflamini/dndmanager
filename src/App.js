06e1749b4ce06db9e055cc003211918f985ff903
root <root@DESKTOP-4L76FGE>
2023-03-31 11:18:20 -0400

    re-pull all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
+import Login from './pages/Login';
+import PlayerList from './pages/PlayerList';
+import CreatePlayer from './pages/CreatePlayer';
+import Player from './pages/Player';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import DungeonMaster from './DM/DungeonMaster';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    //const [player, setPlayer] = useState(null);
+
+    const useSessionStorage = (keyName, defaultValue) => {
+      const [storedValue, setStoredValue] = useState(() => {
+        try {
+          const value = window.sessionStorage.getItem(keyName);
+    
+          if (value) {
+            return JSON.parse(value);
+          } else {
+            window.sessionStorage.setItem(keyName, JSON.stringify(defaultValue));
+            return defaultValue;
+          }
+        } catch (err) {
+          return defaultValue;
+        }
+      });
+    
+      const setValue = newValue => {
+        try {
+          window.sessionStorage.setItem(keyName, JSON.stringify(newValue));
+        } catch (err) {}
+        setStoredValue(newValue);
+      };
+    
+      return [storedValue, setValue];
+    };
+
+    const [player, setPlayer] = useSessionStorage('player', null);
+    
+    const router = createBrowserRouter([
+        {
+          path: "/",
+          element: <Login />,
+        },
+        {
+          path: "/playerlist",
+          element: <PlayerList setPlayer={setPlayer}/>
+        },
+        {
+          path: "/createplayer",
+          element: <CreatePlayer />
+        },
+        {
+          path: "/player",
+          element: <Player player={player}/>
+        },
+        {
+          path: "/dm",
+          element: <DungeonMaster />
+        }
+    ])
+
+    return (
+        <div className="App">
+            <RouterProvider router={router} />
+        </div>
+    );
+    
 }
-
-export default App;
